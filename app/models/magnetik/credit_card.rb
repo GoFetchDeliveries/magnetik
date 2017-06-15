69629d03dd51941c5f9a6606b803af2c2c228f7d
Anh Dao <frankensteinruo@gmail.com>
2017-06-15 11:22:09 +1000

    Removed max name length restriction.

--- app/models/magnetik/credit_card.rb
+++ app/models/magnetik/credit_card.rb
@@ -10,7 +10,7 @@ module Magnetik
     validates :exp_month, presence: true
     validates :exp_year, presence: true
     validates :brand, presence: true
-    validates :name, length: { maximum: Magnetik.max_name_length }
+    validates :name, presence: true
     validates :last_validated_at, presence: true
 
     def requires_revalidation?
